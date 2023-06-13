3614e7a02d5471cacd2321695fbd242c69d08d61
TheHayonicDev <108293666+TheHayonicDev@users.noreply.github.com>
2023-06-13 18:49:39 +0100

    Update redirect-scripts1.js

--- redirect-scripts1.js
+++ redirect-scripts1.js
@@ -1,7 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
     const countdownElement = document.querySelector('.countdown_text');
-    const continueButton = document.getElementById('continueButton');
-
     let count = 5;
 
     function updateCountdown() {
@@ -11,10 +9,8 @@ document.addEventListener('DOMContentLoaded', function () {
         if (count < 0) {
             clearInterval(countdownInterval);
             countdownElement.textContent = 'Done!';
-            continueButton.classList.remove('disableInteraction');
         }
     }
 
     const countdownInterval = setInterval(updateCountdown, 1000);
-    continueButton.classList.add('disableInteraction');
 });
